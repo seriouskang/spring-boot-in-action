874b9fbba6ffe9c959e25b783ff4fbd37b13f170
seriouskang <scdean110@gmail.com>
2023-07-26 21:54:08 +0900

    feat(http-request): http-request 프로젝트 추가

---
+++ http-request/src/main/java/com/example/controller/RequestController.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+public class RequestController {
+    @GetMapping("/client-ip")
+    public String clientIp(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+        log.info("X-FORWARDED-FOR : " + ip);
+
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+            log.info("Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+            log.info("WL-Proxy-Client-IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+            log.info("HTTP_CLIENT_IP : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            log.info("HTTP_X_FORWARDED_FOR : " + ip);
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+            log.info("getRemoteAddr : "+ip);
+        }
+        log.info("Result : IP Address : "+ip);
+
+        return ip;
+    }
+}
