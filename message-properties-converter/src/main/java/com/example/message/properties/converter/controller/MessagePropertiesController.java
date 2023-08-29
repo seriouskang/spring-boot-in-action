ccfdc89263be16e0d565c102cbe7b852943e480d
seriouskang <scdean110@gmail.com>
2023-08-30 00:11:50 +0900

    feat(message-properties-converter): init

---
+++ message-properties-converter/src/main/java/com/example/message/properties/converter/controller/MessagePropertiesController.java
@@ -0,0 +1,48 @@
+package com.example.message.properties.converter.controller;
+
+import com.example.message.properties.converter.io.MessagePropertiesHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/message/properties")
+@RequiredArgsConstructor
+public class MessagePropertiesController {
+    private final MessagePropertiesHandler messagePropertiesHandler;
+
+    @GetMapping
+    public String messageList(Model model) {
+        model.addAttribute("koMessage", getMessageOf("message.properties"));
+        model.addAttribute("enMessage", getMessageOf("message_en.properties"));
+        // @TODO: validate
+
+        return "message/list";
+    }
+
+    @PutMapping("/en")
+    public String modifyEnglishMessage(@RequestParam Map<String, String> modifiedEnMessage) throws IOException {
+        cleanseMessage(modifiedEnMessage);
+        log.info("modifiedEnMessage = {}", modifiedEnMessage);
+
+        messagePropertiesHandler.saveMessages("message_en.properties", modifiedEnMessage);
+        return "redirect:/message/list";
+    }
+
+    private void cleanseMessage(Map<String, String> message) {
+        message.remove("_method");
+    }
+
+    private Map<String, String> getMessageOf(String propertiesFileName) {
+        return messagePropertiesHandler.getMessages(propertiesFileName);
+    }
+}
---
+++ message-properties-converter/src/main/java/com/example/message/properties/converter/io/MessagePropertiesHandler.java
@@ -0,0 +1,55 @@
+package com.example.message.properties.converter.io;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class MessagePropertiesHandler {
+    public Map<String, String> getMessages(String propertiesFilePath) {
+        // @TODO: 경로 수정
+        try(LineIterator lineIterator = FileUtils.lineIterator(new File("message-properties-converter/src/main/resources/" + propertiesFilePath))) {
+            Map<String, String> messages = new LinkedHashMap<>();
+
+            while(lineIterator.hasNext()) {
+                String readLine = lineIterator.nextLine();
+                if(!readLine.contains("=")) continue;
+                messages.put(keyOf(readLine), valueOf(readLine));
+            }
+
+            log.info("messages = {}", new ObjectMapper().writeValueAsString(messages));
+            return messages;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveMessages(String propertiesFilePath, Map<String, String> messages) {
+        // @TODO: 경로 수정
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/" + propertiesFilePath, true))) {
+            for(Map.Entry<String, String> entry: messages.entrySet()) {
+                writer.write(entry.getKey() + "=" + entry.getValue());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String keyOf(String line) {
+        return line.split("=", -1)[0].trim();
+    }
+
+    private String valueOf(String line) {
+        return line.split("=", -1)[1].trim();
+    }
+}
