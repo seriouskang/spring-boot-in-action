5e5be34699f4005a310bf14ba0d848fd5ac6981f
kdh <dh.kang@koreacb.com>
2023-09-05 13:29:45 +0900

    feat(MessageProperty): errorMessage 필드 추가

--- message-properties-converter/src/main/java/com/example/message/properties/converter/domain/MessageProperty.java
+++ message-properties-converter/src/main/java/com/example/message/properties/converter/domain/MessageProperty.java
@@ -2,15 +2,18 @@ package com.example.message.properties.converter.domain;
 
 import lombok.*;
 
-import javax.validation.constraints.NotBlank;
-
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class MessageProperty {
     private String key;
     private String koValue;
-    @NotBlank
     private String enValue;
+    private String errorMessage;
+
+    public MessageProperty(String key, String koValue, String enValue) {
+        this.key = key;
+        this.koValue = koValue;
+        this.enValue = enValue;
+    }
 }
