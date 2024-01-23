32d28d07283405df5b3c7b55d58aa343a7333b5d
seriouskang <scdean110@gmail.com>
2024-01-23 13:45:24 +0900

    feat(opentelemetry-setup:upstream): ProductControllerAdvice 메소드 추가(HttpClientErrorException, HttpServerErrorException)

--- opentelemetry-setup/upstream/src/main/java/com/example/exception/ProductControllerAdvice.java
+++ opentelemetry-setup/upstream/src/main/java/com/example/exception/ProductControllerAdvice.java
@@ -4,11 +4,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 @RestControllerAdvice
 public class ProductControllerAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException exception) {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<Object> handleHttpClientErrorException(HttpClientErrorException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpServerErrorException.class)
+    public ResponseEntity<Object> handleHttpServerErrorException(HttpServerErrorException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
