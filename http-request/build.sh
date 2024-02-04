c91670c6b8a2c174bc260a7df7b188fd69a77090
seriouskang <scdean110@gmail.com>
2024-02-04 22:34:59 +0900

    build(http-request): 멀티 플랫폼 빌드 관련 빌드 스크립트 수정

--- http-request/build.sh
+++ http-request/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 ../gradlew build
-docker build --tag http-request:0.1 .
+# docker build --tag http-request:0.1 .
+docker buildx build --push \
+                    --platform linux/arm64/v8,linux/amd64 \
+                    --tag scdean/http-request:0.2 .
