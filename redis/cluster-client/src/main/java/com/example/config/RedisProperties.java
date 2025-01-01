90ca6ae347b823cc3080e4ffa476ea8e96c4b35d
seriouskang <scdean110@gmail.com>
2025-01-01 23:20:44 +0900

    feat(redis-cluster-client): RedisProperties 추가

---
+++ redis/cluster-client/src/main/java/com/example/config/RedisProperties.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "spring.data.redis.cluster")
+public class RedisProperties {
+    private final int maxRedirects;
+    private final List<String> nodes;
+}
