ffc2a4ba3143febd92d0e93efb8e65c5bad1ac80
seriouskang <scdean110@gmail.com>
2025-01-01 23:28:34 +0900

    feat(redis-cluster-client): RedisConfig 추가

---
+++ redis/cluster-client/src/main/java/com/example/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@RequiredArgsConstructor
+@Configuration
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration(redisProperties.getNodes());
+        redisClusterConfiguration.setMaxRedirects(redisProperties.getMaxRedirects());
+
+        return new LettuceConnectionFactory(redisClusterConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
--- redis/cluster-client/src/main/java/com/example/config/RedisProperties.java
+++ redis/cluster-client/src/main/java/com/example/config/RedisProperties.java
@@ -1,10 +1,12 @@
 package com.example.config;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
+@Getter
 @RequiredArgsConstructor
 @ConfigurationProperties(prefix = "spring.data.redis.cluster")
 public class RedisProperties {
