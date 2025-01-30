f7474dbbce6ea24d2c56b2e9dc492864eb19fbad
seriouskang <scdean110@gmail.com>
2025-01-30 19:54:01 +0900

    refactor(opentelemetry-sample-app): lombok 적용

--- opentelemetry-sample-app/src/main/java/com/example/demo/controller/Controller.java
+++ opentelemetry-sample-app/src/main/java/com/example/demo/controller/Controller.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,25 +14,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+@Slf4j
 @RestController
 public class Controller {
-    Logger logger = LoggerFactory.getLogger(Controller.class);
-
     @GetMapping("/")
     public String root(@RequestParam(value = "name", defaultValue = "World") String name, @RequestHeader HttpHeaders headers) {
-        logger.error(headers.toString());
-        logger.error(String.format("Hello %s!!", name));
-        logger.debug("Debugging log");
-        logger.info("Info log");
-        logger.warn("Hey, This is a warning!");
-        logger.error("Oops! We have an Error. OK");
+        log.error(headers.toString());
+        log.error(String.format("Hello %s!!", name));
+        log.debug("Debugging log");
+        log.info("Info log");
+        log.warn("Hey, This is a warning!");
+        log.error("Oops! We have an Error. OK");
         return String.format("Hello %s!!", name);
     }
 
     @GetMapping("/io_task")
     public String io_task() throws InterruptedException {
         Thread.sleep(1000);
-        logger.info("io_task");
+        log.info("io_task");
         return "io_task";
     }
 
@@ -42,14 +40,14 @@ public class Controller {
         for (int i = 0; i < 100; i++) {
             int tmp = i * i * i;
         }
-        logger.info("cpu_task");
+        log.info("cpu_task");
         return "cpu_task";
     }
 
     @GetMapping("/random_sleep")
     public String random_sleep() throws InterruptedException {
         Thread.sleep((int) (Math.random() / 5 * 10000));
-        logger.info("random_sleep");
+        log.info("random_sleep");
         return "random_sleep";
     }
 
@@ -59,7 +57,7 @@ public class Controller {
         Random rand = new Random();
         int randomElement = givenList.get(rand.nextInt(givenList.size()));
         response.setStatus(randomElement);
-        logger.info("random_status");
+        log.info("random_status");
         return "random_status";
     }
 
@@ -67,14 +65,14 @@ public class Controller {
     public String chain() {
         String TARGET_ONE_HOST = System.getenv().getOrDefault("TARGET_ONE_HOST", "localhost");
         String TARGET_TWO_HOST = System.getenv().getOrDefault("TARGET_TWO_HOST", "localhost");
-        logger.debug("chain is starting");
+        log.debug("chain is starting");
 
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getForEntity("http://localhost:8080/", String.class);
         restTemplate.getForEntity(String.format("http://%s/io_task", TARGET_ONE_HOST), String.class);
         restTemplate.getForEntity(String.format("http://%s/cpu_task", TARGET_TWO_HOST), String.class);
 
-        logger.debug("chain is finished");
+        log.debug("chain is finished");
         return "chain";
     }
 
