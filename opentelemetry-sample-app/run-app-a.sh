6c35b9721619baa841b9e6368e9f0b31cf493dd6
seriouskang <scdean110@gmail.com>
2025-02-02 22:17:54 +0900

    refactor(opentelemetry-sample-app): container run script 수정(OTEL 관련 환경변수)

--- opentelemetry-sample-app/run-app-a.sh
+++ opentelemetry-sample-app/run-app-a.sh
@@ -13,9 +13,10 @@ podman run --rm -d --name app-a \
            -e TARGET_TWO_HOST=192.168.64.129:28080 \
            -e MANAGEMENT_METRICS_TAGS_APPLICATION=app-a \
            -e OTEL_METRICS_EXPORTER=none \
-           -e OTEL_LOGS_EXPORTER=otlp \
-           -e OTEL_EXPORTER_OTLP_ENDPOINT=http://192.168.64.128:4317 \
+           -e OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://192.168.64.128:4318/v1/traces \
+           -e OTEL_EXPORTER_OTLP_TRACES_PROTOCOL=http/protobuf \
            -e OTEL_EXPORTER_OTLP_LOGS_ENDPOINT=http://192.168.64.128:4318/v1/logs \
            -e OTEL_EXPORTER_OTLP_LOGS_PROTOCOL=http/protobuf \
+           -e OTEL_LOGS_EXPORTER=otlp \
            -e OTEL_RESOURCE_ATTRIBUTES=service.name=app-a \
            localhost/opentelemetry-sample-app:latest
