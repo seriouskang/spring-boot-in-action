8cf007df2368370216947b743215a8a8805a2405
seriouskang <scdean110@gmail.com>
2025-08-20 20:22:49 +0900

    feat(http-request): header API 추가

--- http-request/src/main/java/com/example/controller/RequestController.java
+++ http-request/src/main/java/com/example/controller/RequestController.java
@@ -1,15 +1,12 @@
 package com.example.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -17,31 +14,37 @@ public class RequestController {
     @GetMapping("/client-ip")
     public String clientIp(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
-        log.info("X-FORWARDED-FOR : {}", ip);
+        log.info("  > X-FORWARDED-FOR : {}", ip);
 
         if (ip == null) {
             ip = request.getHeader("Proxy-Client-IP");
-            log.info("Proxy-Client-IP : {}", ip);
+            log.info("  > Proxy-Client-IP : {}", ip);
         }
         if (ip == null) {
             ip = request.getHeader("WL-Proxy-Client-IP");
-            log.info("WL-Proxy-Client-IP : {}", ip);
+            log.info("  > WL-Proxy-Client-IP : {}", ip);
         }
         if (ip == null) {
             ip = request.getHeader("HTTP_CLIENT_IP");
-            log.info("HTTP_CLIENT_IP : {}", ip);
+            log.info("  > HTTP_CLIENT_IP : {}", ip);
         }
         if (ip == null) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-            log.info("HTTP_X_FORWARDED_FOR : {}", ip);
+            log.info("  > HTTP_X_FORWARDED_FOR : {}", ip);
         }
         if (ip == null) {
             ip = request.getRemoteAddr();
-            log.info("getRemoteAddr : {}", ip);
+            log.info("  > getRemoteAddr : {}", ip);
         }
-        log.info("Result : IP Address : {}", ip);
+        log.info("Result : {}", ip);
 
         return ip;
+    }
+
+    @GetMapping("/headers")
+    public Map<String, String> headers(@RequestHeader Map<String, String> headers) {
+        log.info("headers = {}", headers);
+        return headers;
     }
 
     @GetMapping("/hostname")
